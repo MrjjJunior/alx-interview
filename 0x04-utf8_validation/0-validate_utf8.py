c8d94057384bc7ef50c457194edfa22ae9da9984
Tshepiso <tlhongtshepiso@gmail.com>
2024-10-31 16:12:34 +0000

    utf8 validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" utf8 validation """
+
+
+def validUTF8(data):
+    """ utf8 validation """
+    bytes_remain = 0
+
+    for number in data:
+        byte = number & 0xFF
+
+        if bytes_remain == 0:
+            if (byte >> 5) == 0b110:
+                bytes_remain = 1
+            elif (byte >> 4) == 0b1110:
+                bytes_remain = 2
+            elif (byte >> 3) == 0b11110:
+                bytes_remain = 3
+            elif (byte >> 7):
+                return False
+        else:
+            if (byte >> 6) != 0b10:
+                return False
+            bytes_remain -=1
+    
+    return bytes_remain == 0
