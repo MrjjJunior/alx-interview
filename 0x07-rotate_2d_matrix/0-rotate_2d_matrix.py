3abf3738d6879b3b22447a8d8a2a47a9ec98bb7d
Tshepiso <tlhongtshepiso2@gmail.com>
2024-11-21 22:04:00 +0200

    90* rotation

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,11 +6,9 @@ def rotate_2d_matrix(matrix):
     ''' Rotate 90 degrees clockwise '''
     length = len(matrix)
 
-    for integer in range(length):
-        for sec_integer in range(integer, length):
-            swap1 = matrix[sec_integer][integer], matrix[integer][sec_integer]
-            swap2 = matrix[integer][sec_integer], matrix[sec_integer][integer]
-            swap1 = swap2
+    for i in range(length):
+        for s in range(i, length):
+            matrix[s][i], matrix[i][s] = matrix[i][s], matrix[s][i]
 
     for integer in range(length):
         matrix[integer] = matrix[integer][::-1]
